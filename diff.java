450e638a5cf69fc3fc43fbf2320a1a14bf38dc9d
Vedikaa <mandlik.vedikaa16688@gmail.com>
2024-01-18 10:05:47 +0530

    adding diff.java

--- diff.java
+++ diff.java
@@ -2,8 +2,7 @@ public class Sum {
     public static void main(String[] args){
         int a = 5;
         int b = 6;
-        int c = 10;
 
-        System.out.println(a+b+c);
+        System.out.println(a-b);
     }
 }
